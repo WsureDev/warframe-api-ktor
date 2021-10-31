f66fb5f5c8bf39e627c9ebdd28a9fd6cc33a59da
Wsure <root@wsure.top>
2021-10-31 22:54:36 +0800

    dict download

--- src/main/kotlin/top/wsure/top/service/Warframe.kt
+++ src/main/kotlin/top/wsure/top/service/Warframe.kt
@@ -1,11 +1,12 @@
 package top.wsure.top.service
 
-import top.wsure.top.utils.downloadDict
-import top.wsure.top.utils.downloadInvasion
-import top.wsure.top.utils.downloadNightWave
+import top.wsure.top.utils.*
 
 suspend fun initDictFollow(){
     val dictList = downloadDict()
     val invasionList = downloadInvasion()
     val nightWaveList = downloadNightWave()
+    val libList = downloadLib()
+    val saleList = downloadSale()
+    val rivenList = downloadRiven()
 }
--- src/main/kotlin/top/wsure/top/utils/DictUtils.kt
+++ src/main/kotlin/top/wsure/top/utils/DictUtils.kt
@@ -1,7 +1,6 @@
 package top.wsure.top.utils
 
-import top.wsure.top.dto.Dict
-import top.wsure.top.dto.DictEnum
+import top.wsure.top.dto.*
 
 suspend fun downloadDict(): List<Dict> {
     return DictEnum.DICT.remoteUrl.getJsonObject() ?: DictEnum.DICT.customUrl.readResourceJson()
@@ -16,5 +15,20 @@ suspend fun downloadInvasion(): List<Dict> {
 suspend fun downloadNightWave(): List<Dict> {
     return DictEnum.NIGHT_WAVE.remoteUrl.getJsonObject() ?: DictEnum.NIGHT_WAVE.customUrl.readResourceJson()
     ?: DictEnum.NIGHT_WAVE.localUrl.readResourceJson() ?: emptyList()
+}
+
+suspend fun downloadLib(): List<LibItem> {
+    return DictEnum.LIB.remoteUrl.getJsonObject() ?: DictEnum.LIB.customUrl.readResourceJson()
+    ?: DictEnum.LIB.localUrl.readResourceJson() ?: emptyList()
+}
+
+suspend fun downloadSale(): List<Sale> {
+    return DictEnum.SALE.remoteUrl.getJsonObject() ?: DictEnum.SALE.customUrl.readResourceJson()
+    ?: DictEnum.SALE.localUrl.readResourceJson() ?: emptyList()
+}
+
+suspend fun downloadRiven(): List<Riven> {
+    return DictEnum.RIVEN.remoteUrl.getJsonObject() ?: DictEnum.RIVEN.customUrl.readResourceJson()
+    ?: DictEnum.RIVEN.localUrl.readResourceJson() ?: emptyList()
 }
 
